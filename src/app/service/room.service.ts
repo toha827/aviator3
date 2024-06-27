b6f894b0b867fe5f071d3b72844dadab9db9ba5d
rau4o <rauan.k@eventyr.pro>
2024-06-27 19:15:58 +0500

    commit

--- src/app/service/room.service.ts
+++ src/app/service/room.service.ts
@@ -1,6 +1,6 @@
 import {inject, Injectable} from "@angular/core";
 import {HttpClient, HttpHeaders} from "@angular/common/http";
-import {Observable} from "rxjs";
+import {BehaviorSubject, Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +9,33 @@ import {Observable} from "rxjs";
 export class RoomService {
   private readonly baseUrl = 'http://209.250.233.190:80';
   private http = inject(HttpClient);
+
+  public checkIsAuto$: BehaviorSubject<any> = new BehaviorSubject<any>(null);
+  public checkShowOver: BehaviorSubject<any> = new BehaviorSubject<any>(null);
+
+  public getBalance(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/v1/bets/balance`);
+  }
+
+  public setBalance(body: {amount: number}): Observable<any> {
+    return this.http.post(`${this.baseUrl}/v1/bets/set-balance`, body);
+  }
+
+  public getIsCheckedAuto() {
+    return this.checkIsAuto$.asObservable();
+  }
+
+  public setIsCheckedAuto(value: any): void {
+    this.checkIsAuto$.next(value);
+  }
+
+  public setCoeff(value: any) {
+    this.checkShowOver.next(value);
+  }
+
+  public getCoeff() {
+    return this.checkShowOver.asObservable();
+  }
 
   public getRoomsList(): Observable<any> {
     return this.http.get(`${this.baseUrl}/v1/rooms`);
@@ -27,7 +54,7 @@ export class RoomService {
   }
 
   public withdraw(): Observable<any> {
-    return this.http.get(`${this.baseUrl}v1/bets/withdraw`);
+    return this.http.get(`${this.baseUrl}/v1/bets/withdraw`);
   }
 
   public cancelBet(id: number): Observable<any> {
