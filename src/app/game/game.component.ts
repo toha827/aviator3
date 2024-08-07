83edd9f1b5b7e9ca3a5f5228030cee29d81db414
Aktilek <actilek.m@livin.kz>
2024-08-07 16:02:45 +0500

    refactor

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -421,8 +421,8 @@ export class GameComponent implements OnInit, OnDestroy {
 
       // Check if we have passed a 2s threshold and adjust duration
       if (totalDuration >= stepThreshold) {
-        if (currentDuration - stepDecrement <= 0.1) {
-          currentDuration = 0.1;
+        if (currentDuration - stepDecrement <= 1) {
+          currentDuration = 1;
           stepThreshold += 1000.0;  // update the next threshold
           continue;
         } else {
